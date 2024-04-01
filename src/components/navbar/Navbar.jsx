2186b08d0074a677a1dba110e618b257886cab8d
antoinehage612 <hypercoffin612@gmail.com>
2024-04-01 12:02:52 +0300

    completed navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav>
       <div className="left">
@@ -14,11 +18,28 @@ function Navbar() {
         <a href="/">Agents</a>
       </div>
       <div className="right">
-        <a href="/">Sign in</a>
-        <a href="/" className="register">
-          Sign up
-        </a>
-        <div className="menuIcon"></div>
+        <>
+          <a href="/">Sign in</a>
+          <a href="/" className="register">
+            Sign up
+          </a>
+        </>
+
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
